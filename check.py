4b2ef6e864ce560de8a412ce5e2351fd81bcc2a9
xiao-gy <wgy20050420@gmail.com>
2021-04-15 21:57:49 +0800

    增加了可选的check功能(其实就是再尝试下载一次)

---
+++ check.py
@@ -0,0 +1,4 @@
+import os
+
+for i in list(os.walk(os.path.join(os.getcwd(),'bz')))[0][1]:
+    os.system('aria2c --conf-path=./config/aria2.conf -d '+os.path.join(os.getcwd(),'bz',i)+' -i '+os.path.join(os.getcwd(),'bz',i,'file.txt'))
