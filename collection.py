e623ed468084029b34b7d11dc869051e2e920035
xiao-gy <wgy20050420@gmail.com>
2021-05-03 21:56:37 +0800

    收藏本子

---
+++ collection.py
@@ -0,0 +1,11 @@
+import os
+
+from get_detail import get_bzdetail
+from download_aria2 import download_aria2
+
+def collection(opt):
+    if opt == '1':
+        pass
+
+if __name__ == "__main__":
+    collection(input())
