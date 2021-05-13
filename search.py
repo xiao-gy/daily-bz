2134d7297593c2a8932d29c6df53534242a30c22
xiao-gy <wgy20050420@gmail.com>
2021-05-13 17:59:10 +0800

    修复结果只有一页时的出锅

--- search.py
+++ search.py
@@ -11,7 +11,10 @@ def search(headers,url_base,keyword):
     global url_list
     r = requests.get(url_base+'/search/q_'+keyword,headers=headers)
     html = etree.HTML(r.text)
-    page_sum = int(html.xpath('//*[@id="content"]/section/span[@class="last"]/a/@data-ci-pagination-page')[0])
+    try:
+        page_sum = int(html.xpath('//*[@id="content"]/section/span[@class="last"]/a/@data-ci-pagination-page')[0])
+    except Exception:
+        page_sum = 1
     page = int(input('共 '+str(page_sum)+' 页,请输入你要下载的页数: '))
     for i in range(min(page,page_sum)):
         r = requests.get(url_base+'/search/q_'+keyword+'/page/'+str(i+1),headers=headers)
@@ -26,4 +29,4 @@ if __name__ == "__main__":
     headers= {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'
     }
-    print(search(headers,'https://zhb.eehentai.com/','C97'))
+    print(search(headers,'https://zhb.eehentai.com/','泳装'))
