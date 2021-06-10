2c76cbd99b517821d3c26a695e44a7c122e75713
xiao-gy <wgy20050420@gmail.com>
2021-06-10 21:59:07 +0800

    更新防暴毙

--- check.py
+++ check.py
@@ -2,10 +2,18 @@ import os
 import json
 
 from download import download_form
+from get import get_bzdetail
+from setting import info
 
 def check():
+    version,headers,url_base,t_tag = info()
     for i in list(os.walk(os.path.join(os.getcwd(),'bz')))[0][1]:
-        f = open(os.path.join(os.getcwd(),'bz',i,'info.json'), mode='r',encoding="utf-8")
+        try:
+            f = open(os.path.join(os.getcwd(),'bz',i,'info.json'), mode='r',encoding="utf-8")
+        except Exception:
+            url_add = '/g/'+i+'/'
+            get_bzdetail(headers,url_base,url_add)
+            f = open(os.path.join(os.getcwd(),'bz',i,'info.json'), mode='r',encoding="utf-8")
         data = dict(json.load(f))
         if not data['state'] == 'checked':
             os.system('aria2c --conf-path=./config/aria2.conf -d '+os.path.join(os.getcwd(),'bz',i)+' -i '+os.path.join(os.getcwd(),'bz',i,'file.txt'))
